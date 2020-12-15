4e62d7bf0c64e8dfeb275e72d0de3fe718f52da4
amar <avadla@gemini-us.com>
2020-12-15 20:32:26 +0530

    added home

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,39 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import './Navbar.css'
 
 function Navbar() {
+
+    const [rows, setRows] = useState([]);
+
+
+    useEffect(() => {
+        let rowElem = [];
+        for (let i = 0; i < 25; i++) {
+            rowElem.push(<div className="card" key={i}>
+                <h2>Hello</h2>
+                <p>sdlkfjslkdjflsjlfkjdljsflksjldfjlldkjf</p>
+            </div>)
+        }
+        setRows(rowElem);
+    }, [])
+
     return (
-        <div>
-            
-        </div>
+        <>
+            <div className="top-nav">
+                <div className="left">
+                    <a href="/home"> HOME  </a>
+                    <a href="/shop"> SHOP  </a>
+                    <a href="/buy"> BUY  </a>
+                    <a href="/sel"> SELL  </a>
+                </div>
+                <div className="contact">
+                    <a href="/contact"> CONTACT US  </a>
+                </div>
+            </div>
+            <div className="grid-layout">
+                {rows}
+            </div>
+        </>
     )
 }
 
